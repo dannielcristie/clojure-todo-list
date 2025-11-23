469eaff5b66bf3c366972680e7360805952d00b7
Danniel Cristie <dannielcristie@gmail.com>
2025-11-23 16:58:55 -0300

    feat: conecta frontend com API do backend (CORS corrigido)

--- src/todo/backend/core.clj
+++ src/todo/backend/core.clj
@@ -4,6 +4,7 @@
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.cors :refer [wrap-cors]]
             [todo.backend.handler :as handler])
   (:gen-class))
 
@@ -22,7 +23,9 @@
   (ring/ring-handler
    app-routes
    (ring/create-default-handler)
-   {:middleware [wrap-json-response
+   {:middleware [[wrap-cors :access-control-allow-origin [#"http://localhost:8000"]
+                             :access-control-allow-methods [:get :post :put :delete]]
+                 wrap-json-response
                  [wrap-json-body {:keywords? true}]
                  wrap-keyword-params
                  wrap-params]}))
